18019a1b1142ff3150cfc977c24e71bb1031ec10
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-10-07 13:51:33 +1100

    updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
-  # After application_controller, write the before_action in users_controller
   before_action :check_if_logged_in, :only => [:edit]
   before_action :check_if_admin, :only => [:index]
 
   # A page for index.html.erb
   def index
     @users = User.all
+    render json: @users.to_json
   end
 
   # A page for new.html.erb
@@ -17,14 +17,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
 
-    # if params[:user][:password].empty?
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
-      # (Go back to the home page)
+      render :json => @user
     else
-      render :new
-      # Show them the new(form) again
+      render :json => @user.errors
     end
   end
 
@@ -36,7 +33,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    # user = User.find params[:id]
     user = @current_user
 
     # cloudinary = Cloudinary::Uploader.upload( params["user"]["userPhoto"] )
@@ -46,8 +42,17 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def whoami
+    render :json => @current_user
+  end
+
 
   private
+  # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :location, :name)
   end
