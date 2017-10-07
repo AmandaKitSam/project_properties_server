919198b2dd14382e096d1b1be28fbd9a7b022980
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-10-07 22:58:53 +1100

    updated controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,19 @@ class UsersController < ApplicationController
   before_action :check_if_logged_in, :only => [:edit]
   before_action :check_if_admin, :only => [:index]
 
+
   # A page for index.html.erb
   def index
     @users = User.all
     render json: @users.to_json
   end
 
+
   # A page for new.html.erb
   def new
     @user = User.new
   end
+
 
   def create
     @user = User.new user_params
@@ -25,12 +28,13 @@ class UsersController < ApplicationController
     end
   end
 
+
   # A page for edit.html.erb
   def edit
     redirect_to root_path unless @current_user.id == params[:id].to_i
-    # Make sure no one can edit other's profile / .to_i means turn it to integers(numbers)
     @user = User.find params[:id]
   end
+
 
   def update
     user = @current_user
@@ -41,6 +45,7 @@ class UsersController < ApplicationController
     user.update user_params
     redirect_to root_path
   end
+
 
   def whoami
     render :json => @current_user
