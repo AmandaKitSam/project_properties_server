6cdb8c402c85bedc2f6f05ab8475650e14d2eb6e
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-10-07 13:53:42 +1100

    updated Session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,20 @@
 class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by :email => params[:email]
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render :json => user
+    else
+      flash[:error] = "Invalid email or password"
+      # redirect_to login_path # don't need this ?
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    # redirect_to root_path # don't need this ?
+  end
 end
