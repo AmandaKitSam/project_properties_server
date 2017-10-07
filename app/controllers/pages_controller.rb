f8fde5e1d7cdd0e50ce625c5038c750eb03be1f2
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-10-07 14:07:19 +1100

    updated pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   def home
     if @current_user.present?
-      @diary = Diary.new
+      # @photo = Photo.new
       @user = @current_user
       render :json => {
-        :diary => @diary,
+        # :photo => @photo,
         :user => @user
       }
     end
