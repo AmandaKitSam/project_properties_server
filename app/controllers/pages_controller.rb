dfe28791269bfe6619767981874fde2a575a6879
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-10-07 14:02:12 +1100

    updated pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,12 @@
 class PagesController < ApplicationController
+  def home
+    if @current_user.present?
+      @diary = Diary.new
+      @user = @current_user
+      render :json => {
+        :diary => @diary,
+        :user => @user
+      }
+    end
+  end
 end
