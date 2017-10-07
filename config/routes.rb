eb0ee410d6a3a7a78f6a84a2b7baab4d8eee8585
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-10-07 15:02:43 +1100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+
+  # user login
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+
+  get '/whoami' => 'users#whoami'
 end
